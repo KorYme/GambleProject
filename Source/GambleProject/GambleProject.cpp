cb468d172d119a9d66cf53fa151f0e0c03c3bca9
KorYme <maxime.corby.pro@gmail.com>
2026-06-12 00:10:44 +0200

    [GPP] : Init project

---
+++ Source/GambleProject/GambleProject.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GambleProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GambleProject, "GambleProject" );
+
+DEFINE_LOG_CATEGORY(LogGambleProject)
